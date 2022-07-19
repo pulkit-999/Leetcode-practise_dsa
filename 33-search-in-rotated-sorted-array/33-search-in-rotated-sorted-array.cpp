1a12580bf682e0418274c702e25a058ab5293f6f
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-07-19 10:28:47 +0530

    Time: 7 ms (52.53%), Space: 12.1 MB (29.04%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]=i;
+        }
+        if(m.find(target)!=m.end())
+        {
+            return m[target];
+        }
+            else
+                return -1;
+    }
+};
