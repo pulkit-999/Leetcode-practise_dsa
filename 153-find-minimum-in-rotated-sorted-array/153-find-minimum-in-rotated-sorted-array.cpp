89f05a031b80ebfb32eb32898d9a36efedc69d43
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-07-28 10:29:18 +0530

    Time: 4 ms (71.01%), Space: 10.2 MB (22.73%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -13,7 +13,8 @@ public:
         if(nums[hi] >= nums[0]) return nums[0];
         while(lo<=hi) {
              mid = lo + (hi-lo) / 2;
-            if(nums[mid] <= nums[(mid+N-1) % N]) return nums[mid];
+            if(nums[mid] <= nums[(mid+N-1) % N] && nums[mid] <= nums[(mid+1) % N]) 
+                return nums[mid];
             else if(nums[mid] >= nums[0]) lo = mid+1;
             else hi = mid-1;
         }
