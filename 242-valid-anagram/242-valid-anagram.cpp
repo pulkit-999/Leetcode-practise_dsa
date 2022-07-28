f8bdd99338bdb47845dd5db6faf71cae1b611dff
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-07-28 18:34:28 +0530

    Time: 17 ms (44.89%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        sort(s.begin(),s.end());
-         sort(t.begin(),t.end());
-        if(s.size()!=t.size())
-            return false;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]!=t[i])
-            return false;
-        }
-        return true;
+       unordered_map<char,int> m;
+    
+  if(s.size()!=t.size()) 
+        return false;
+  for(int i =0;i<s.size();i++)
+  {
+      m[s[i]]++;
+      m[t[i]]--;
+  }
+   for(auto i:m)
+  {
+     if(i.second!=0)
+        return false;
+  }
+  return true;
     }
 };
