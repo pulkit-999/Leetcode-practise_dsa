d500db89611135ef1ecfaf5f9550d79a7bae587e
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-07-31 16:52:41 +0530

    Time: 56 ms (60.26%), Space: 8.4 MB (57.23%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<int>& ds,int &maxi,int ind,int &count)
+    {
+        if(ind==nums.size())
+        {
+            int cur=0;
+            for(auto x:ds)
+            {
+                cur=cur|x;
+            }
+            if(cur==maxi)
+            {
+                count++;
+            }
+            return;
+        }
+        ds.push_back(nums[ind]);
+        solve(nums,ds,maxi,ind+1,count);
+             ds.pop_back();
+        solve(nums,ds,maxi,ind+1,count);
+    }
+    int countMaxOrSubsets(vector<int>& nums) 
+    {
+        int maxi=0;
+        int count=0;
+        vector<int>ds;
+        for(auto x:nums)
+        {
+            maxi=maxi|x;
+        }
+     solve(nums,ds,maxi,0,count);
+        return count;
+    }
+};
