f512d0c15506156034ad44de137e0c8db366775f
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-08-13 23:39:31 +0530

    Time: 3 ms (79.92%), Space: 5.8 MB (89.25%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int solve(int step,int value,int copy,int n,vector<vector<int>>&dp)
-    {
-        if(step>n||value>n)
-        {
-            return 1e9;
-        }
-        if(value==n)
-            return step;
-        if(dp[step][value]!=-1)
-            return dp[step][value];
-    return dp[step][value] =min(solve(step+1,copy+value,copy,n,dp),solve(step+2,2*value,value,n,dp));
+    // int solve(int step,int value,int copy,int n,vector<vector<int>>&dp)
+//     {
+//         if(step>n||value>n)
+//         {
+//             return 1e9;
+//         }
+//         if(value==n)
+//             return step;
+//         if(dp[step][value]!=-1)
+//             return dp[step][value];
+//     return dp[step][value] =min(solve(step+1,copy+value,copy,n,dp),solve(step+2,2*value,value,n,dp));
          
-    }
+//     }
     int minSteps(int n) 
     {
-        if(n==1)
-            return 0;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-    return solve(1,1,1,n,dp);
+        int res = 0;
+        for (int i = 2; i <= n / i; i++) {
+            while (n % i == 0) {
+                n /= i;
+                res += i;
+            }
+        }
+        
+        if (n > 1) res += n;
+        return res;
     }
 };
