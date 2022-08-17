0bd22c8a92c0021db25127b4097bfeba5bfdc173
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-08-17 14:28:35 +0530

    Time: 8 ms (34.91%), Space: 8.5 MB (88.09%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -3,14 +3,14 @@ public:
     int uniqueMorseRepresentations(vector<string>& words) 
     {
     string arr[26]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        map<char,string>m;
+        //map<char,string>m;
         int i=0;
         unordered_set<string>s;
-        for(char ch='a';ch<='z';ch++)
-        {
-            m[ch]=arr[i];
-            i++;
-        }
+        // for(char ch='a';ch<='z';ch++)
+        // {
+        //     m[ch]=arr[i];
+        //     i++;
+        // }
         string res="";
         for(int i=0;i<words.size();i++)
         {
@@ -18,7 +18,7 @@ public:
             string k=words[i];
             for(int j=0;j<k.size();j++)
             {
-                res=res+m[k[j]];
+                res=res+arr[k[j]-'a'];
                           }
             if(res!="")
             s.insert(res);
