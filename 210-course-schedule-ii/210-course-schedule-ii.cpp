4add749564e8de43c9156050351848fd00bdb095
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-08-22 17:28:40 +0530

    Time: 24 ms (85.58%), Space: 13.2 MB (94.90%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+     queue<int>q;
+        vector<int>graph[numCourses];
+        vector<int>ans;
+        vector<int>indegree(numCourses,0);
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            graph[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
+        }
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        while(!q.empty())
+        {
+             int el=q.front();
+            ans.push_back(el);
+            q.pop();
+            for(auto y:graph[el])
+            {
+                indegree[y]--;
+                if(indegree[y]==0)
+                    q.push(y);
+            }
+        }
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]!=0)
+            {
+                ans.clear();
+                return ans;
+                }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
