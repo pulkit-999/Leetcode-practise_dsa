a9091d57dc362ddddedd86bbaf00b46d5b3dd0b2
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-08-24 12:57:44 +0530

    Time: 266 ms (74.52%), Space: 67.4 MB (57.83%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,31 +1,10 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_set<int>c;
-    for(auto x:edges)
-    {
-        c.insert(x[0]);
-        c.insert(x[1]);
-    }
-    int n=c.size();
-    int ans=0;
-vector<int>indegree(n+1,0);
-vector<int>outdegree(n+1,0);
-for(int i=0;i<edges.size();i++)
-{
-    indegree[edges[i][1]]++;
-    outdegree[edges[i][0]]++;
-}
-int maxi=0;
-for(int i=1;i<=n;i++)
-{
-    if(indegree[i]+outdegree[i]>maxi)
-    {
-        maxi=indegree[i]+outdegree[i];
-        ans=i;
-    }
-    
-}
-return ans;
-    }
+        int a=edges[0][0],b=edges[0][1];
+        if(a==edges[1][0]) return a;
+        else if(b==edges[1][0]) return b;
+        else if(b==edges[1][1]) return b;
+        else if(a==edges[1][1]) return a;
+        return 1;}
 };
