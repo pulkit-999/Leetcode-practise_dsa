752a587b3ce4780f3578294e3d6d52ecc383c495
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-08-25 23:29:06 +0530

    Time: 275 ms (5.03%), Space: 49 MB (5.14%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,44 +1,51 @@
 class Solution {
 public:
     #define ll long long
-    #define mod 1000000007
-    int countPaths(int n, vector<vector<int>>& roads) {
-        using pi = pair<ll,ll>;
-        ll min_dis = LONG_MAX;
-        vector<vector<ll>> adj[n];
+    const int mod=1e9+7;
+    int countPaths(int n, vector<vector<int>>& roads)
+    {
+        vector<vector<long long>>graph[n];
+        // for(int i=0;i<n;i++)
+        // {
+        //     graph[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+        //     graph[roads[i][1]].push_back({roads[i][0],roads[i][2]});
+        // }
         for(vector<int> a:roads){
             ll u = a[0];
             ll v = a[1];
             ll wt = a[2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+            graph[u].push_back({v,wt});
+            graph[v].push_back({u,wt});
         }
+      
+    vector<long long>dis(n,LONG_MAX),ways(n,0);
+        ways[0]=1;
+        dis[0]=0;
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>pq;
+        pq.push({0,0});
         
-        vector<ll> dist(n,LONG_MAX);
-        vector<ll> ways(n);
-        dist[0] = 0;
-        ways[0] = 1;
-        priority_queue<pi,vector<pi>,greater<pi>> q;
-        q.push({0,0});
-        while(!q.empty()){
-            pair<ll,ll> p = q.top();
-            q.pop();
-            ll cur_dis = p.first;
-            ll cur_node = p.second;
-        
-            for(auto arr : adj[cur_node]){
-                ll new_dis = cur_dis + arr[1];
-                ll new_node = arr[0];
-                
-                if(new_dis < dist[new_node]){
-                    dist[new_node] = new_dis;
-                    q.push({new_dis,new_node});
-                    ways[new_node] = ways[cur_node];
-                }else if(new_dis == dist[new_node]){
-                    ways[new_node] = (ways[cur_node]%mod + ways[new_node])%mod;
+        while(!pq.empty())
+        {
+            int prev=pq.top().second;
+            auto dist=pq.top().first;
+            pq.pop();
+           
+            for(auto arr : graph[prev]){
+                ll distt = dist + arr[1];
+                ll next = arr[0];
+                if(dis[next]>distt)
+                {
+                   
+                    dis[next]=distt;
+                    pq.push({dis[next],next});
+                     ways[next]=ways[prev];
+                }
+                else if(distt==dis[next])
+                {
+                    ways[next]=(ways[next]%mod+ways[prev]%mod);
                 }
             }
-        }    
-        return ways[n-1];
+        }
+        return ways[n-1]%mod;
     }
 };
