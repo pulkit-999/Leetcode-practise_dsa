11c5c78e8fcfd5d8adc7cc79e2c4fdea7e4a7ef7
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-08-26 19:53:11 +0530

    Time: 24 ms (56.74%), Space: 8.7 MB (89.81%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       vector<int>ans(26,0);
+        for(int i=0;i<ransomNote.size();i++)
+        {
+            ans[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<magazine.size();i++)
+        {
+            ans[magazine[i]-'a']--;
+        }
+        
+         for(int i=0;i<26;i++)
+        {
+            if(ans[i]>0)
+                return false;
+        }
+        return true;
+        
+    }
+};
