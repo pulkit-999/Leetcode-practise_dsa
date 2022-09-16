c26bd3a0c3e79aa4c5941dcadd780059203c837b
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2022-09-16 20:32:24 +0530

    Time: 928 ms (22.44%), Space: 120 MB (68.36%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(vector<vector<int>>&dp,vector<int>& nums, vector<int>& multipliers,int i,int j,int n)
+    {
+        if(j==multipliers.size())
+            return 0;
+        if(dp[i][j]!=-1e9)
+            return dp[i][j];
+        int front=solve(dp,nums,multipliers,i+1,j+1,n)+nums[i]*multipliers[j];
+        int back=solve(dp,nums,multipliers,i,j+1,n)+nums[n-1-(j-i)]*multipliers[j];
+        return dp[i][j]=max(front,back);
+    }
+    int maximumScore(vector<int>& nums, vector<int>& multipliers)
+    {
+        int m=multipliers.size();
+       vector<vector<int>>dp(m+1,vector<int>(m+1,-1e9));
+       return solve(dp,nums,multipliers,0,0,nums.size());
+    }
+};
