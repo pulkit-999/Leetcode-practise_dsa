76de5caed55c5ba5d7db6167b2f62ef54f446eca
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2023-09-04 16:41:49 +0530

    Time: 2 ms (57.94%), Space: 6 MB (43.71%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int solve(vector<int>&dp,int n)
+{
+    if(n<=1)
+    return 1;
+    if(dp[n]!=-1)
+    return dp[n];
+    int ans=0;
+    for(int i=1;i<=n;i++)
+    {
+        ans+=solve(dp,i-1)*solve(dp,n-i);
+    }
+    return dp[n]=ans;
+}
+    int numTrees(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(dp,n);
+    }
+};
