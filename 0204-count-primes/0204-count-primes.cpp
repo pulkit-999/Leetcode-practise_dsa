cbf1264b5ba98a8fb9d6026d70844a27c6580b09
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2023-09-05 17:20:44 +0530

    Time: 136 ms (93.04%), Space: 10.4 MB (21.61%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>prime(n+1,1);
+        for(int p=2;p*p<n;p++)
+        {
+            if(prime[p]==true)
+            {
+                for(int i=p*p;i<=n;i+=p)
+                {
+                    prime[i]=false;
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=2;i<n;i++)
+        {
+
+            if(prime[i])
+            cnt++;
+
+        }
+        return cnt;
+    }
+};
