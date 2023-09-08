20057dbeff47c0c00a7221f811461df30a1daf4b
pulkit-999 <97938993+pulkit-999@users.noreply.github.com>
2023-09-08 19:39:33 +0530

    Time: 0 ms (100%), Space: 7.2 MB (18.61%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+     int cnt=0;
+    
+     int maxi=-1;
+     for(int i=0;i<arr.size();i++)
+     {
+        maxi=max(maxi,arr[i]);
+        if(maxi==i)
+        cnt++;
+    }
+    return cnt;
+    }
+};
